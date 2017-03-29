81774d7794783e63947971cea45ad0272be6354c
robinlietar <robinlietar@MacBook-Pro-de-Robin-2.local>
2017-03-29 13:07:45 +0200

    Revert "Removed error in Soda_db get_pendings function"
    This reverts commit a4e763fbc75502b519216136ff3247373084f05a.

--- sofa_db.py
+++ sofa_db.py
@@ -576,7 +576,7 @@ class DBManager():
 
     def get_pendings(self, name, pwd):
         session = Session(self.engine)
-        pendings = session.query(User, Film)
+        pendings = session.qYuery(User, Film)
         pendings = pendings.filter(User.pending.any(Film.id))
         pendings = pendings.filter(User.name==name).filter(User.pwd==pwd)
         return [p[1] for p in pendings]
@@ -592,3 +592,4 @@ dbm = DBManager()
 
 #print(dbm.add_user('Rob','pwc'))
 print(dbm.check_log_in_info('Robin','pwc'))
+
